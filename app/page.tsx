48272db62123eb0ba15428b1f66bc97e26767b39
rainclover8 <ts.baran6161@gmail.com>
2025-08-19 14:31:08 +0300

    refactor: improve layout and styling in Home component for better user experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Tags from "./components/Tags";
 import Card from "./components/ui/Card";
 import MainNavbar from "./components/ui/Navbar";
 import ProjectsCard from "./components/ui/ProjectsCard";
+import { FaCode } from "react-icons/fa6";
 
 export default function Home() {
 
@@ -10,35 +11,63 @@ export default function Home() {
 
   return (
     <>
-      <div className="">
-       <div className="sticky top-2">
-         <MainNavbar />
-       </div>
+      <div className="h-full">
+        <div className="sticky top-2">
+          <MainNavbar />
+        </div>
 
         <HeaderSection />
 
         <div className="lg:w-1/2 px-6 mx-auto mt-24">
           <Card />
+          <div className="mt-12 mb-12 w-full" >
+            <a href="#project" className="bg-blue-700 px-4 py-2 rounded-lg flex w-fit mx-auto hover:bg-blue-500 justify-center items-center gap-2"> <FaCode />Projeleri Incele</a>
+          </div>
         </div>
-        <div className="flex flex-col items-center mt-12 md:mt-24 px-4">
-  {/* Başlık */}
-  <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
-    <span className="me-2">📂</span>Tamamlanan Projeler
-  </h1>
-  <small className="text-sm sm:text-base text-gray-400 mt-2 text-center">
-    Şimdiye kadar tamamladığım projeler
-  </small>
 
-  {/* Kartlar */}
-  <div className="flex flex-wrap justify-center gap-4 sm:gap-6 mt-8 md:mt-12 w-full">
-    <ProjectsCard 
-      projectDay={"Gün 3"} 
-      projectName={"YouTube Subscriber Counter"} 
-      projectDetail={"Gerçek zamanlı YouTube abone sayacı uygulaması"} 
-      tags={["React", "YouTube API"]} 
-    />
-  </div>
-</div>
+
+
+        <div className="flex flex-col items-center mt-12 md:mt-36 px-4">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
+            <span className="me-2">📂</span>Tamamlanan Projeler
+          </h1>
+          <small className="text-sm sm:text-base text-gray-400 mt-2 text-center">
+            Şimdiye kadar tamamladığım projeler
+          </small>
+
+          {/* Kartlar */}
+          <div className="flex flex-wrap justify-center gap-4 sm:gap-6 mt-8 md:mt-12 w-full" id="project">
+            <ProjectsCard
+              projectDay={"Gün 3"}
+              projectName={"YouTube Subscriber Counter"}
+              projectDetail={"Gerçek zamanlı YouTube abone sayacı uygulaması"}
+              tags={["React", "YouTube API"]}
+            />
+          </div>
+        </div>
+
+        {/* Gelecek Projeler */}
+        <div className="mt-24 text-center">
+          <h1 className="text-4xl font-bold mb-4">🚀 Gelecek Projeler</h1>
+          <small className="text-sm text-gray-400 ">Yakında gelecek heyecan verici projeler</small>
+
+          {/* Card yapısı */}
+          <div className="">
+
+          </div>
+        </div>
+
+        {/* About Kısmı */}
+        <div className="">
+
+        </div>
+
+        {/* Footer */}
+        <footer>
+
+        </footer>
+
+        {/* Hepsini ayır */}
 
       </div>
     </>
