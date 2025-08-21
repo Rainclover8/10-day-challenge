c0746b4837b914cf872808a4d1faed5d9ac85fec
rainclover8 <ts.baran6161@gmail.com>
2025-08-21 23:00:05 +0300

    style: improve code formatting for HeaderSection and Tags components

--- app/components/HeaderSection.tsx
+++ app/components/HeaderSection.tsx
@@ -2,19 +2,25 @@ import { CiCalendar } from "react-icons/ci";
 
 function HeaderSection() {
   return (
-    <section className='mt-12'>
-
+    <section className="mt-12">
       <div className="flex items-center justify-evenly gap-x-2 text-[#436cc8] bg-gray-400 w-fit mx-auto px-3 py-2 rounded-full">
-        <span className=''><CiCalendar size={20} /></span>
+        <span className="">
+          <CiCalendar size={20} />
+        </span>
         <h6>Gün 5 / 10</h6>
       </div>
 
       <div className="text-center mt-12">
-        <h1 className=' md:text-7xl text-2xl font-bold bg-gradient-to-r from-blue-700 to-purple-300 bg-clip-text text-transparent p-2 '>10 Günde AirPods Challenge</h1>
-        <h6 className='md:text-2xl text-sm md:w-1/2 mx-auto md:mt-7 mt-3'>Her gün yeni bir proje yaparak hedefime doğru ilerliyorum. Gün gün projeleri buradan takip edebilirsiniz.</h6>
+        <h1 className=" md:text-7xl text-2xl font-bold bg-gradient-to-r from-blue-700 to-purple-300 bg-clip-text text-transparent p-2 ">
+          10 Günde AirPods Challenge
+        </h1>
+        <h6 className="md:text-2xl text-sm md:w-1/2 mx-auto md:mt-7 mt-3">
+          Her gün yeni bir proje yaparak hedefime doğru ilerliyorum. Gün gün
+          projeleri buradan takip edebilirsiniz.
+        </h6>
       </div>
     </section>
-  )
+  );
 }
 
-export default HeaderSection
+export default HeaderSection;
--- app/components/Tags.tsx
+++ app/components/Tags.tsx
@@ -1,16 +1,17 @@
-
 interface TagsProps {
-    techStack: string[];
+  techStack: string[];
 }
 
 function Tags({ techStack }: TagsProps) {
-    return (
-        <div className="flex gap-3">
-            {techStack.map((tech, index) => (
-                <span key={index} className='mt-4 border w-fit px-2 rounded-full'>{tech}</span>
-            ))}
-            </div>
-    )
+  return (
+    <div className="flex gap-3">
+      {techStack.map((tech, index) => (
+        <span key={index} className="mt-4 border w-fit px-2 rounded-full">
+          {tech}
+        </span>
+      ))}
+    </div>
+  );
 }
 
-export default Tags
+export default Tags;
